f01e5092692bb2ad68324cd5d5a081f079500bdd
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-08 18:54:41 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,22 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
+variable "name" {
+  description = "name of MWAA"
   type        = string
-  default     = "ExampleAppServerInstance"
-}
+  }
+variable "security_group_ids" {
+  description = "security group ids"
+  type        = list(string)
+  }
+variable "subnet_ids" {
+  description = "subnet ids"
+  type        = list(string)
+  }
+
+variable "execution_role_arn" {
+  description = "subnet ids"
+  type        = string
+  }
+
+variable "execution_role_arn" {
+  description = "subnet ids"
+  type        = string
+  }
